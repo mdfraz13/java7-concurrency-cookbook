5da414ede1da6079502016ce9526591bd6fa31a3
Matei Dragu <tatacalu@users.noreply.github.com>
2013-09-21 14:39:12 +0200

    TCNumberUtils - INT_ONE should be 1

--- src/main/java/ro/tatacalu/java7concurrency/ch01/util/TCNumberUtils.java
+++ src/main/java/ro/tatacalu/java7concurrency/ch01/util/TCNumberUtils.java
@@ -10,7 +10,7 @@ package ro.tatacalu.java7concurrency.ch01.util;
 public class TCNumberUtils {
     
     public static final int INT_ZERO = 0;
-    public static final int INT_ONE = 0;
+    public static final int INT_ONE = 1;
     
     public static final long LONG_ONE = 1L;
 }
