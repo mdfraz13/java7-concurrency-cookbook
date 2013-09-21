173987658d01c2102de9466c9cf3b7cb7d684dbb
Matei Dragu <tatacalu@users.noreply.github.com>
2013-09-21 14:24:21 +0200

    Chapter 1 - Example #9: ThreadLocal variables

---
+++ src/main/java/ro/tatacalu/java7concurrency/ch01/MainUnsafeTask.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package ro.tatacalu.java7concurrency.ch01;
+
+import java.util.concurrent.TimeUnit;
+
+import ro.tatacalu.java7concurrency.ch01.util.TCNumberUtils;
+
+/**
+ * @author Matei
+ *
+ */
+public class MainUnsafeTask {
+
+    private static final int I_END = 10;
+    private static final long LONG_SLEEP_INTERVAL = 2L;
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        UnsafeTask unsafeTask = new UnsafeTask();
+        for (int i = TCNumberUtils.INT_ZERO; i < I_END; i++) {
+            Thread thread = new Thread(unsafeTask);
+            thread.start();
+            
+            try {
+                TimeUnit.SECONDS.sleep(LONG_SLEEP_INTERVAL);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        
+    }
+
+}
---
+++ src/main/java/ro/tatacalu/java7concurrency/ch01/UnsafeTask.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package ro.tatacalu.java7concurrency.ch01;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Matei
+ *
+ */
+public class UnsafeTask implements Runnable {
+
+    private static final String STRING_THREAD_START = "Starting Thread: %s : %s\n";
+    private static final String STRING_THREAD_END = "Thread Finished: %s : %s\n";
+    
+    private Date startDate;
+    
+    /* (non-Javadoc)
+     * @see java.lang.Runnable#run()
+     */
+    @Override
+    public void run() {
+        this.startDate = new Date();
+        
+        System.out.printf(STRING_THREAD_START, Thread.currentThread().getId(), this.startDate);
+        
+        long sleepTime = (long) Math.rint(Math.random() * 10);
+        try {
+            TimeUnit.SECONDS.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        System.out.printf(STRING_THREAD_END, Thread.currentThread().getId(), this.startDate);
+    }
+
+}
