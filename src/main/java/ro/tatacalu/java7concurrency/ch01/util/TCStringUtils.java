c158e97a7e40953c53034cf18dcf16bf5b179b3b
Matei Dragu <tatacalu@users.noreply.github.com>
2013-09-21 15:43:24 +0200

    TCStringUtils class

---
+++ src/main/java/ro/tatacalu/java7concurrency/ch01/util/TCStringUtils.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package ro.tatacalu.java7concurrency.ch01.util;
+
+/**
+ * @author Matei
+ *
+ */
+public class TCStringUtils {
+    
+    public static final String NL = System.lineSeparator();
+    
+}
